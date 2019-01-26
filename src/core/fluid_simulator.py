4a582bd4587f7eda8ffab89dfe859e2f7262d6fd
Federico Bertola <fb@bendingspoons.com>
2019-01-26 03:37:42 +0100

    Refactoring

--- src/core/fluid_simulator.py
+++ src/core/fluid_simulator.py
@@ -16,18 +16,18 @@ class FluidSimulator:
     PRESSURE_WRITE = 1
 
     # Compute Shader Kernel Ids
-    _add_velocity_kernel = 0
-    _init_boundaries_kernel = 0
-    _advect_velocity_kernel = 0
-    _divergence_kernel = 0
-    _poisson_kernel = 0
-    _subtract_gradient_kernel = 0
-    _calc_vorticity_kernel = 0
-    _apply_vorticity_kernel = 0
-    _viscosity_kernel = 0
-    _add_circle_obstacle_kernel = 0
-    _add_triangle_obstacle_kernel = 0
-    _clear_buffer_kernel = 0
+    _add_velocity_kernel = None
+    _init_boundaries_kernel = None
+    _advect_velocity_kernel = None
+    _divergence_kernel = None
+    _poisson_kernel = None
+    _subtract_gradient_kernel = None
+    _calc_vorticity_kernel = None
+    _apply_vorticity_kernel = None
+    _viscosity_kernel = None
+    _add_circle_obstacle_kernel = None
+    _add_triangle_obstacle_kernel = None
+    _clear_buffer_kernel = None
 
     _velocity_buffer = None
     _divergence_buffer = None
@@ -41,8 +41,6 @@ class FluidSimulator:
     _width = 512
     _height = 512
 
-    _last_frame_time = 0.0
-
     # Public properties
     speed = 500.0
     iterations = 50
@@ -52,81 +50,16 @@ class FluidSimulator:
     resolution = 512
     simulate = True
 
-    def __init__(self, context: Context, time_delta_func):
-        self.time_delta_func = time_delta_func
+    def __init__(self, context: Context):
         self.context = context
 
-        self._add_velocity_kernel = context.compute_shader(
-            read_shader_source("shader.AddVelocity.comp")
-        )
-        self._init_boundaries_kernel = context.compute_shader(
-            read_shader_source("shader.InitBoundaries.comp")
-        )
-        self._advect_velocity_kernel = context.compute_shader(
-            read_shader_source("shader.AdvectVelocity.comp")
-        )
-        self._divergence_kernel = context.compute_shader(
-            read_shader_source("shader.Divergence.comp")
-        )
-        self._poisson_kernel = context.compute_shader(
-            read_shader_source("shader.Poisson.comp")
-        )
-        self._subtract_gradient_kernel = context.compute_shader(
-            read_shader_source("shader.SubtractGradient.comp")
-        )
-        self._calc_vorticity_kernel = context.compute_shader(
-            read_shader_source("shader.CalcVorticity.comp")
-        )
-        self._apply_vorticity_kernel = context.compute_shader(
-            read_shader_source("shader.ApplyVorticity.comp")
-        )
-        self._add_circle_obstacle_kernel = context.compute_shader(
-            read_shader_source("shader.AddCircleObstacle.comp")
-        )
-        self._add_triangle_obstacle_kernel = context.compute_shader(
-            read_shader_source("shader.AddTriangleObstacle.comp")
-        )
-        self._clear_buffer_kernel = context.compute_shader(
-            read_shader_source("shader.ClearBuffer.comp")
-        )
-        self._viscosity_kernel = context.compute_shader(
-            read_shader_source("shader.Viscosity.comp")
-        )
+        self._load_compute_kernels()
+        self._create_buffers()
+        self._init_compute_kernels()
 
-        self._velocity_buffer = [
-            self._create_vector_buffer(),
-            self._create_vector_buffer(),
-        ]
-        self._pressure_buffer = [
-            self._create_point_buffer(),
-            self._create_point_buffer(),
-        ]
-        self._divergence_buffer = self._create_point_buffer()
-        self._vorticity_buffer = self._create_point_buffer()
-        self._obstacles_buffer = self._create_vector_buffer()
-
-        self._velocity_buffer[self.VELOCITY_READ].bind_to_storage_buffer(
-            TemplateConstants.VELOCITY_IN.value
-        )
-        self._velocity_buffer[self.VELOCITY_WRITE].bind_to_storage_buffer(
-            TemplateConstants.VELOCITY_OUT.value
-        )
-        self._pressure_buffer[self.PRESSURE_READ].bind_to_storage_buffer(
-            TemplateConstants.PRESSURE_IN.value
-        )
-        self._pressure_buffer[self.PRESSURE_WRITE].bind_to_storage_buffer(
-            TemplateConstants.PRESSURE_OUT.value
-        )
-        self._divergence_buffer.bind_to_storage_buffer(
-            TemplateConstants.DIVERGENCE.value
-        )
-        self._vorticity_buffer.bind_to_storage_buffer(TemplateConstants.VORTICITY.value)
-        self._obstacles_buffer.bind_to_storage_buffer(TemplateConstants.OBSTACLES.value)
-
-    def set_size(self, width, height):
+    def set_size(self, width: int, height: int):
         group_size_x = TemplateConstants.NUM_THREADS.value
         group_size_y = TemplateConstants.NUM_THREADS.value
-        group_size_z = 1
 
         self._width = width
         self._height = height
@@ -134,34 +67,17 @@ class FluidSimulator:
         self._num_groups_x = int(ceil(float(width) / float(group_size_x)))
         self._num_groups_y = int(ceil(float(height) / float(group_size_y)))
 
-    def add_velocity(self, position, velocity, radius):
+    def add_velocity(self, position: tuple, velocity: tuple, radius: float):
         self._add_velocity_kernel["_Position"].value = position
         self._add_velocity_kernel["_Value"].value = velocity
         self._add_velocity_kernel["_Radius"].value = radius
-        self._add_velocity_kernel["_Size"].value = (self._width, self._height)
 
         self._add_velocity_kernel.run(self._num_groups_x, self._num_groups_y, 1)
-
         self._flip_velocity_buffer()
 
-    def _update_params(self):
-        delta_time = self.time_delta_func()
-
-        self._advect_velocity_kernel["_ElapsedTime"].value = delta_time
-        self._advect_velocity_kernel["_Speed"].value = self.speed
-
-        self._apply_vorticity_kernel["_ElapsedTime"].value = delta_time
-        self._apply_vorticity_kernel["_VorticityScale"].value = self.vorticity
-
-        centre_factor = 1.0 / self.viscosity
-        stencil_factor = 1.0 / (4.0 + centre_factor)
-
-        self._viscosity_kernel["_Alpha"].value = centre_factor
-        self._viscosity_kernel["_rBeta"].value = stencil_factor
-
-    def update(self):
+    def update(self, time_delta: float):
         if self.simulate:
-            self._update_params()
+            self._update_params(time_delta)
 
             # Init boundaries
             self._init_boundaries_kernel.run(self._num_groups_x, self._num_groups_y, 1)
@@ -170,11 +86,10 @@ class FluidSimulator:
             self._advect_velocity_kernel.run(self._num_groups_x, self._num_groups_y, 1)
             self._flip_velocity_buffer()
 
-            # Vorticity confinement
-            # 1) Calculate vorticity
+            # Vorticity confinement 1 - Calculate vorticity
             self._calc_vorticity_kernel.run(self._num_groups_x, self._num_groups_y, 1)
 
-            # 2) Apply vorticity force
+            # Vorticity confinement 2 - Apply vorticity force
             self._apply_vorticity_kernel.run(self._num_groups_x, self._num_groups_y, 1)
             self._flip_velocity_buffer()
 
@@ -217,6 +132,105 @@ class FluidSimulator:
             self._obstacles_buffer.bind_to_storage_buffer(
                 TemplateConstants.OBSTACLES.value
             )
+
+    def _update_params(self, time_delta: float):
+
+        self._advect_velocity_kernel["_ElapsedTime"].value = time_delta
+        self._advect_velocity_kernel["_Speed"].value = self.speed
+
+        self._apply_vorticity_kernel["_ElapsedTime"].value = time_delta
+        self._apply_vorticity_kernel["_VorticityScale"].value = self.vorticity
+
+        centre_factor = 1.0 / self.viscosity
+        stencil_factor = 1.0 / (4.0 + centre_factor)
+
+        self._viscosity_kernel["_Alpha"].value = centre_factor
+        self._viscosity_kernel["_rBeta"].value = stencil_factor
+
+    def _init_compute_kernels(self):
+        size = (self._width, self._height)
+
+        self._add_velocity_kernel["_Size"].value = size
+        self._init_boundaries_kernel["_Size"].value = size
+        self._advect_velocity_kernel["_Size"].value = size
+        self._divergence_kernel["_Size"].value = size
+        self._poisson_kernel["_Size"].value = size
+        self._subtract_gradient_kernel["_Size"].value = size
+        self._calc_vorticity_kernel["_Size"].value = size
+        self._apply_vorticity_kernel["_Size"].value = size
+        self._add_circle_obstacle_kernel["_Size"].value = size
+        self._add_triangle_obstacle_kernel["_Size"].value = size
+        self._clear_buffer_kernel["_Size"].value = size
+        self._viscosity_kernel["_Size"].value = size
+
+        self._velocity_buffer[self.VELOCITY_READ].bind_to_storage_buffer(
+            TemplateConstants.VELOCITY_IN.value
+        )
+        self._velocity_buffer[self.VELOCITY_WRITE].bind_to_storage_buffer(
+            TemplateConstants.VELOCITY_OUT.value
+        )
+        self._pressure_buffer[self.PRESSURE_READ].bind_to_storage_buffer(
+            TemplateConstants.PRESSURE_IN.value
+        )
+        self._pressure_buffer[self.PRESSURE_WRITE].bind_to_storage_buffer(
+            TemplateConstants.PRESSURE_OUT.value
+        )
+        self._divergence_buffer.bind_to_storage_buffer(
+            TemplateConstants.DIVERGENCE.value
+        )
+        self._vorticity_buffer.bind_to_storage_buffer(TemplateConstants.VORTICITY.value)
+        self._obstacles_buffer.bind_to_storage_buffer(TemplateConstants.OBSTACLES.value)
+
+    def _create_buffers(self):
+        self._velocity_buffer = [
+            self._create_vector_buffer(),
+            self._create_vector_buffer(),
+        ]
+        self._pressure_buffer = [
+            self._create_point_buffer(),
+            self._create_point_buffer(),
+        ]
+        self._divergence_buffer = self._create_point_buffer()
+        self._vorticity_buffer = self._create_point_buffer()
+        self._obstacles_buffer = self._create_vector_buffer()
+
+    def _load_compute_kernels(self):
+        self._add_velocity_kernel = self.context.compute_shader(
+            read_shader_source("shader.AddVelocity.comp")
+        )
+        self._init_boundaries_kernel = self.context.compute_shader(
+            read_shader_source("shader.InitBoundaries.comp")
+        )
+        self._advect_velocity_kernel = self.context.compute_shader(
+            read_shader_source("shader.AdvectVelocity.comp")
+        )
+        self._divergence_kernel = self.context.compute_shader(
+            read_shader_source("shader.Divergence.comp")
+        )
+        self._poisson_kernel = self.context.compute_shader(
+            read_shader_source("shader.Poisson.comp")
+        )
+        self._subtract_gradient_kernel = self.context.compute_shader(
+            read_shader_source("shader.SubtractGradient.comp")
+        )
+        self._calc_vorticity_kernel = self.context.compute_shader(
+            read_shader_source("shader.CalcVorticity.comp")
+        )
+        self._apply_vorticity_kernel = self.context.compute_shader(
+            read_shader_source("shader.ApplyVorticity.comp")
+        )
+        self._add_circle_obstacle_kernel = self.context.compute_shader(
+            read_shader_source("shader.AddCircleObstacle.comp")
+        )
+        self._add_triangle_obstacle_kernel = self.context.compute_shader(
+            read_shader_source("shader.AddTriangleObstacle.comp")
+        )
+        self._clear_buffer_kernel = self.context.compute_shader(
+            read_shader_source("shader.ClearBuffer.comp")
+        )
+        self._viscosity_kernel = self.context.compute_shader(
+            read_shader_source("shader.Viscosity.comp")
+        )
 
     def _create_point_buffer(self):
         data = [0.0 for _ in range(self._num_cells)]
@@ -265,6 +279,6 @@ class FluidSimulator:
 
 
 if __name__ == "__main__":
-    simulator = FluidSimulator(moderngl.create_standalone_context(), None)
+    simulator = FluidSimulator(moderngl.create_standalone_context())
     simulator.set_size(10, 10)
     simulator.add_velocity((1, 1), (0.5, 0.5), 1.0)
