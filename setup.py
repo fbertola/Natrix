0bf7cadf7ca4dffbc3bf303124d6c879bc828260
Federico Bertola <federico.bertola88@gmail.com>
2020-05-13 23:09:23 +0200

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -3,23 +3,19 @@
 
 import io
 import os
-import sys
-from shutil import rmtree
 
-from setuptools import find_packages, setup, Command
+from setuptools import find_packages, setup
 
 NAME = 'Natrix'
 DESCRIPTION = 'Fast fluid simulation for python.'
 URL = 'https://github.com/fbertola/Natrix'
-EMAIL = 'fb@bendingspoons.com'
 AUTHOR = 'Federico Bertola'
 REQUIRES_PYTHON = '>=3.6.0'
 VERSION = None
 
 REQUIRED = [
-    "moderngl",
+    "bgfx-python",
     "numpy",
-    "jinja2",
     "python-decouple",
 ]
 
@@ -39,44 +35,6 @@ if not VERSION:
 else:
     about['__version__'] = VERSION
 
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
-
-
 setup(
     name=NAME,
     version=about['__version__'],
@@ -84,7 +42,6 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     author=AUTHOR,
-    author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=find_packages(exclude=('tests',)),
@@ -96,10 +53,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
-    cmdclass={
-        'upload': UploadCommand,
-    },
 )
